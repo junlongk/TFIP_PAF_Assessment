6d5791248530b5a3ca0c116452a7f0a8d9f780b2
Jun Long <keejunlong@hotmail.com>
2023-03-03 09:17:16 +0800

    Initial set-up

---
+++ Fund_Transfer_App/src/main/java/sg/edu/nus/iss/fund_transfer_app/FundTransferAppApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.fund_transfer_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundTransferAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundTransferAppApplication.class, args);
+    }
+
+}
---
+++ Fund_Transfer_App/src/test/java/sg/edu/nus/iss/fund_transfer_app/FundTransferAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.fund_transfer_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundTransferAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
